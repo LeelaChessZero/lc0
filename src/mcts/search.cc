b899139bdc423e3161052387e16178b3cf09b108
Alexander Lyashuk <crem@google.com>
2018-04-11 20:29:11 +0200

    Don't show overflow eval when checkmate.

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -230,7 +230,7 @@ void Search::SendUciInfo() {
   uci_info_.nodes = total_nodes_;
   uci_info_.nps =
       uci_info_.time ? (uci_info_.nodes * 1000 / uci_info_.time) : 0;
-  uci_info_.score = -91 * log(2 / (best_move_node_->q + 1) - 1);
+  uci_info_.score = -191 * log(2 / (best_move_node_->q * 0.99 + 1) - 1);
   uci_info_.pv.clear();
 
   for (Node* iter = best_move_node_; iter; iter = GetBestChild(iter)) {
