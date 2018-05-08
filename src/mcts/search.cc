7642e84793255f3e5328bc7579ec3b5ec29e0144
Alexander Lyashuk <crem@google.com>
2018-05-08 18:52:16 +0200

    Change CPuct to 1.2 (0.6 in lczero scale)

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -51,7 +51,7 @@ const int kSmartPruningToleranceMs = 200;
 void Search::PopulateUciParams(OptionsParser* options) {
   options->Add<IntOption>(kMiniBatchSizeStr, 1, 1024, "minibatch-size") = 256;
   options->Add<IntOption>(kMiniPrefetchBatchStr, 0, 1024, "max-prefetch") = 32;
-  options->Add<FloatOption>(kCpuctStr, 0, 100, "cpuct") = 1.7;
+  options->Add<FloatOption>(kCpuctStr, 0, 100, "cpuct") = 1.2;
   options->Add<FloatOption>(kTemperatureStr, 0, 100, "temperature") = 0.0;
   options->Add<FloatOption>(kTempDecayStr, 0, 1.00, "tempdecay") = 0.0;
   options->Add<BoolOption>(kNoiseStr, "noise", 'n') = false;
