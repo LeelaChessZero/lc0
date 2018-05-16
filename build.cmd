53fab80b48b10705f7fc549d7f2ab52046e577e6
Alexander Lyashuk <crem@google.com>
2018-05-17 00:40:31 +0200

    Support meson build for windows.

--- build.cmd
+++ build.cmd
@@ -1,8 +1,11 @@
 rd /s build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-meson.py build --backend vs2015 --buildtype release
+meson.py build --backend vs2015 --buildtype release ^
+-Dcudnn_libdirs="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.0\lib\x64","C:\dev\cuDNN\cuda\lib\x64" ^
+-Dcudnn_include="C:\dev\cuDNN\cuda\include"
 
+pause
 
 cd build
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ^
