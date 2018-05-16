0137ec44b706366d0b2138a988ffca294b9ce6b2
Alexander Lyashuk <mooskagh@gmail.com>
2018-05-16 21:18:18 +0200

    Cleanup build.sh.

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-CC=${CC:=clang}
-CXX=${CXX:=clang++}
-
 rm -fr build
-CC=${CC} CXX=${CXX} meson build --buildtype release # -Db_ndebug=true
+meson build --buildtype release # -Db_ndebug=true
 # CC=clang CXX=clang++ meson build --buildtype debugoptimized -Db_asneeded=false
 # CC=clang CXX=clang++ meson build --buildtype debug
-cp testdata/* build
 cd build
 ninja
