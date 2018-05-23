debff893e2009b00762f2aaff61af0b883f8f336
John Newlin <jnewlin@google.com>
2018-05-22 19:30:20 -0700

    Flip the board between moves when generating the move list.

--- src/selfplay/game.cc
+++ src/selfplay/game.cc
@@ -76,9 +76,16 @@ void SelfPlayGame::Play(int white_threads, int black_threads) {
 
 std::vector<Move> SelfPlayGame::GetMoves() const {
   std::vector<Move> moves;
+  int count = 0;
+  for (Node* node = tree_[0]->GetCurrentHead();
+	   node != tree_[0]->GetGameBeginNode(); node = node->GetParent()) {
+	++count;
+  }
+  bool flip = (count & 1) == 0;
   for (Node* node = tree_[0]->GetCurrentHead();
        node != tree_[0]->GetGameBeginNode(); node = node->GetParent()) {
-    moves.push_back(node->GetMove());
+    moves.push_back(node->GetMove(flip));
+	flip = !flip;
   }
   std::reverse(moves.begin(), moves.end());
   return moves;
