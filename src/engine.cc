fe782e2ad31b4bbd70b613db1b243babd855c8f8
jjoshua2 <jjoshua2@gmail.com>
2018-06-27 18:21:40 -0400

    Clop time managment update
    2267 games update.

--- src/engine.cc
+++ src/engine.cc
@@ -77,14 +77,14 @@ void EngineController::PopulateOptions(OptionsParser* options) {
   options->Add<ChoiceOption>(kNnBackendStr, backends, "backend") =
       backends.empty() ? "<none>" : backends[0];
   options->Add<StringOption>(kNnBackendOptionsStr, "backend-opts");
-  options->Add<FloatOption>(kSlowMoverStr, 0.0f, 100.0f, "slowmover") = 1.97f;
+  options->Add<FloatOption>(kSlowMoverStr, 0.0f, 100.0f, "slowmover") = 1.94f;
   options->Add<IntOption>(kMoveOverheadStr, 0, 10000, "move-overhead") = 100;
   options->Add<FloatOption>(kTimeCurvePeak, -1000.0f, 1000.0f,
-                            "time-curve-peak") = 24.0f;
+                            "time-curve-peak") = 26.6f;
   options->Add<FloatOption>(kTimeCurveLeftWidth, 0.0f, 1000.0f,
-                            "time-curve-left-width") = 52.8f;
+                            "time-curve-left-width") = 65.8f;
   options->Add<FloatOption>(kTimeCurveRightWidth, 0.0f, 1000.0f,
-                            "time-curve-right-width") = 71.7f;
+                            "time-curve-right-width") = 77.2f;
 
   Search::PopulateUciParams(options);
 
