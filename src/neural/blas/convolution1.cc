d56efea2079bf9f062a6ba8221fff1dce138b63c
Francois <frpays@gmail.com>
2018-06-28 12:52:20 +0200

    convolution1.cc: fixed comments and imports.

--- src/neural/blas/convolution1.cc
+++ src/neural/blas/convolution1.cc
@@ -16,8 +16,8 @@
  along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "neural/blas/convolution1.h"
 #include "neural/blas/blas.h"
+#include "neural/blas/convolution1.h"
 
 namespace lczero {
 
@@ -35,29 +35,32 @@ void Convolution1::Forward(const size_t batch_size, const size_t input_channels,
     //    cblas_sgemm(CblasRowMajor, TransA, TransB, M, N, K, alpha, A, lda, B,
     //                ldb, beta, C, N);
 
-    //             C                               A                     B
+    //             C                          A                     B
     //
-    //           outputs             :=          weights        x      input
+    //           outputs       :=          weights        x      input
     //
-    //   cols:  kSquares (N)                 input_channels (K)      kSquares
-    //   (N)
+    //   cols:  kSquares (N)         input_channels (K)        kSquares(N)
     //
-    //   rows:  output_channels (M)         output_channels (M)
-    //   input_channels (K)
+    //   rows:  output_channels (M)   output_channels (M)  input_channels (K)
 
     const float* batch_input = input + i * kSquares * input_channels;
     float* batch_output = output + i * kSquares * output_channels;
 
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                // M              N         K         alpha
-                (int)output_channels, kSquares, (int)input_channels, 1.0f,
-                // A     lda
-                weights, (int)input_channels,
-                // B    ldb   beta,
-                batch_input, kSquares, 0.0f,
-                // C   ldc
-                batch_output, kSquares);
-
+    cblas_sgemm(CblasRowMajor, // Row major formar
+                CblasNoTrans, // A not transposed
+                CblasNoTrans, // B not transposed
+                (int)output_channels, // M
+                kSquares, // N
+                (int)input_channels, // K
+                1.0f, // Alpha
+                weights, //A
+                (int)input_channels, // lda, leading rank of A
+                batch_input, // B
+                kSquares, // ldb, leading rank of B
+                0.0f, // beta
+                batch_output, // C
+                kSquares); // ldc, leading rank of B
+    
     auto index = 0;
     for (size_t o = 0; o < output_channels; o++) {
       const auto bias = biases[o];
@@ -68,4 +71,4 @@ void Convolution1::Forward(const size_t batch_size, const size_t input_channels,
   }
 }
 
-}  // namespace lczerp
+}  // namespace lczero
