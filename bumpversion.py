7a60ca8841e07a603cd160bfca93b39330fbd66c
Error323 <folkerthuizinga@gmail.com>
2018-07-16 15:25:40 +0200

    create script to bump version

---
+++ bumpversion.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+def extract(git_version):
+    version_array = git_version.split('.')
+    patch_array = version_array[2].split('-')
+    major = int(version_array[0][1:])
+    minor = int(version_array[1])
+    patch = int(patch_array[0])
+    return major, minor, patch
+
+
+def tag(major, minor, patch):
+    version = "v{}.{}.{}".format(major, minor, patch)
+    subprocess.getoutput("git tag {}".format(version))
+
+
+def main(argv):
+    git_version = subprocess.getoutput("git describe --always")
+    major, minor, patch = extract(git_version)
+
+    if argv.major:
+        major += 1
+        minor = 0
+        patch = 0
+        tag(major, minor, patch)
+    elif argv.minor:
+        minor += 1
+        patch = 0
+        tag(major, minor, patch)
+    elif argv.patch:
+        patch += 1
+        tag(major, minor, patch)
+
+    print('v{}.{}.{}'.format(major, minor, patch))
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=\
+            'Set or read current version.')
+    argparser.add_argument('--major', action='store_true',
+            help='bumps major version')
+    argparser.add_argument('--minor', action='store_true',
+            help='bumps minor version')
+    argparser.add_argument('--patch', action='store_true',
+            help='bumps patch')
+    argv = argparser.parse_args()
+    main(argv)
+
