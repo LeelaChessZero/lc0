d33ede25aa276409c91e4c57bfa28c5f0e30016d
Error323 <folkerthuizinga@gmail.com>
2018-07-16 10:46:35 +0200

    release vs dev proposal

--- src/version.cc
+++ src/version.cc
@@ -4,7 +4,9 @@ std::uint32_t GetVersionInt(int major, int minor, int patch) {
   return major * 1000000 + minor * 1000 + patch;
 }
 
-std::string GetVersionStr(int major, int minor, int patch) {
-  return std::to_string(major) + "." + std::to_string(minor) + "." +
-         std::to_string(patch);
+std::string GetVersionStr(int major, int minor, int patch, bool release) {
+  auto v = std::to_string(major) + "." + std::to_string(minor) + "." +
+           std::to_string(patch);
+  if (release) return v;
+  return v + "-dev";
 }
