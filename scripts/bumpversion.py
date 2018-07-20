0dc4cfb5820de86d950f6a4a5918913b85f6d0ae
Error323 <folkerthuizinga@gmail.com>
2018-07-20 10:28:07 +0200

    simplify versioning

--- scripts/bumpversion.py
+++ scripts/bumpversion.py
@@ -1,48 +1,80 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess
+import tempfile
+import textwrap
+import os
+from string import Template
+from subprocess import call
 
-def extract(git_version):
-    version_array = git_version.split('.')
-    patch_array = version_array[2].split('-')
-    major = int(version_array[0][1:])
-    minor = int(version_array[1])
-    patch = int(patch_array[0])
-    postfix = ""
+VERSION_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../src/version.inc")
+VERSION_CONTENT = """
+#define LC0_VERSION_MAJOR $major 
+#define LC0_VERSION_MINOR $minor
+#define LC0_VERSION_PATCH $patch
+#define LC0_VERSION_POSTFIX $postfix
+"""
+VERSION_CONTENT = textwrap.dedent(VERSION_CONTENT).strip()
+EDITOR = os.environ.get("EDITOR", "vim")
 
-    if len(patch_array) == 3:
-        postfix = patch_array[2]
+
+def get_current_version():
+    with open(VERSION_FILE, 'r') as f:
+        major = int(f.readline().split()[2])
+        minor = int(f.readline().split()[2])
+        patch = int(f.readline().split()[2])
+        postfix = f.readline().split()[2]
 
     return major, minor, patch, postfix
 
 
+def set_current_version(major, minor, patch, postfix="\"\""):
+    tmp = Template(VERSION_CONTENT)
+    version_inc = tmp.substitute(major=major, minor=minor, patch=patch, postfix=postfix)
+
+    with open(VERSION_FILE, 'w') as f:
+        f.write(version_inc)
+
+
 def tag(major, minor, patch):
     version = "v{}.{}.{}".format(major, minor, patch)
-    subprocess.getoutput("git tag -m {} {}".format(version, version))
+    call(["git", "-m", version, version])
+
+
+def editor(init_msg):
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(init_msg)
+        f.flush()
+        call([EDITOR, f.name])
+        f.seek(0)
+        return f.read()
+
+
+def update(major, minor, patch):
+    set_current_version(major, minor, patch)
+    # edit changelot
+    # git commit
+    # git tag
 
 
 def main(argv):
-    git_version = subprocess.getoutput("git describe --always")
-    major, minor, patch, postfix = extract(git_version)
+    major, minor, patch, postfix = get_current_version()
 
     if argv.major:
         major += 1
         minor = 0
         patch = 0
         postfix = ""
-        tag(major, minor, patch)
+        update(major, minor, patch)
     elif argv.minor:
         minor += 1
         patch = 0
         postfix = ""
-        tag(major, minor, patch)
+        update(major, minor, patch)
     elif argv.patch:
         patch += 1
         postfix = ""
-        tag(major, minor, patch)
-    elif postfix != "":
-        patch += 1
+        update(major, minor, patch)
 
     print('{} {} {} {}'.format(major, minor, patch, postfix))
 
