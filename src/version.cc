f1acfffce3c67154bf889dd90a4a8a5c7d4179d7
Alexander Lyashuk <crem@google.com>
2018-07-22 13:04:57 +0200

    Don't show patch for -dev and -rc releases. Also rename dev version to be 0.17-dev rather than 0.16.1-dev

--- src/version.cc
+++ src/version.cc
@@ -1,12 +1,17 @@
 #include "version.h"
+#include <cassert>
 
 std::uint32_t GetVersionInt(int major, int minor, int patch) {
   return major * 1000000 + minor * 1000 + patch;
 }
 
-std::string GetVersionStr(int major, int minor, int patch, const std::string& postfix) {
-  auto v = std::to_string(major) + "." + std::to_string(minor) + "." +
-           std::to_string(patch);
-  if (postfix.empty()) return v;
-  return v + "-" + postfix;
+std::string GetVersionStr(int major, int minor, int patch,
+                          const std::string& postfix) {
+  auto v = std::to_string(major) + "." + std::to_string(minor);
+  if (postfix.empty()) {
+    return v + "." + std::to_string(patch);
+  } else {
+    assert(patch == 0);
+    return v + "-" + postfix;
+  }
 }
