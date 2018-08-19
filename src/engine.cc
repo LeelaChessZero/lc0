68f36e53e702757902199d682231289c429cc41c
Jonathan Roy <jonroy7@gmail.com>
2018-08-19 11:27:30 -0400

    Fix typo

--- src/engine.cc
+++ src/engine.cc
@@ -165,7 +165,7 @@ SearchLimits EngineController::PopulateSearchLimits(int ply, bool is_black,
   // Make sure we don't exceed current time limit with what we calculated.
   limits.time_ms = std::max(
       int64_t{0},
-      std::min(static_cast<int64_t>(this_move_time), time std::max(lagbuffer, move_overhead)));
+      std::min(static_cast<int64_t>(this_move_time), time - std::max(lagbuffer, move_overhead)));
   return limits;
 }
 
