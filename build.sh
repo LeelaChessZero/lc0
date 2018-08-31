b673d10001e542fb77fe2f348ecc331d533f0606
borg323 <borg323@users.noreply.github.com>
2018-08-31 18:12:07 +0300

    pass build.sh options to meson

--- build.sh
+++ build.sh
@@ -2,20 +2,23 @@
 
 set -e
 
-BUILDTYPE=$1
-
-if [ -z "${BUILDTYPE}" ]
-then
-  BUILDTYPE=release
-fi
+case $1 in
+  plain|debug|debugoptimized|release|minsize)
+    BUILDTYPE=$1
+    shift
+    ;;
+  *)
+    BUILDTYPE=release
+    ;;
+esac
 
 BUILDDIR=build/${BUILDTYPE}
 
 if [ -d ${BUILDDIR} ]
 then
-  meson configure ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local}
+  meson configure ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local} "$@"
 else
-  meson ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local}
+  meson ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local} "$@"
 fi
 
 pushd ${BUILDDIR}
