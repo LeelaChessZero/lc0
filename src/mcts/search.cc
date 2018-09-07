63750091af87c23be691787de52cc1bbf965290f
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2018-09-06 22:22:01 -0400

    Another code optimization thanks to @sergiovieri.

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -470,9 +470,9 @@ EdgeAndNode Search::GetBestChildWithTemperature(Node* parent,
 
     if (edge.GetN() <= static_cast<unsigned int>(kMinimumTemperatureVisits)) {
       accepted_edges.push_back(edge);
+    } else {
+      all_edges.push_back(edge);
     }
-
-    all_edges.push_back(edge);
   }
 
   if (accepted_edges.empty()) {
