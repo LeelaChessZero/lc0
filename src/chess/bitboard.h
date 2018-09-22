fe2dce9c71ab6ea7f58a2b188d09c5bde8d67a07
Daniel Uranga <daniel.uranga@gmail.com>
2018-09-22 14:44:00 -0300

    Add Win32-compatible version of "popcount" and "bitscan" (#327)
    * Make population count function Win32-compatible
    
    * Make bit scan function Win32-compatible
    
    * Fix a bug catched during review
    
    * Rearrange preprocessor directives to avoid nested #ifdef blocks

--- src/chess/bitboard.h
+++ src/chess/bitboard.h
@@ -104,8 +104,10 @@ class BitBoard {
     x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333);
     x = (x + (x >> 4)) & 0x0F0F0F0F0F0F0F0F;
     return (x * 0x0101010101010101) >> 56;
-#elif defined(_MSC_VER)
+#elif defined(_MSC_VER) && defined(_WIN64)
     return _mm_popcnt_u64(board_);
+#elif defined(_MSC_VER)
+    return __popcnt(board_) + __popcnt(board_ >> 32);
 #else
     return __builtin_popcountll(board_);
 #endif
