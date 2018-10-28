e4340b246bf100ac172a5a3b1adcdee4e296e26b
Alexander Lyashuk <mooskagh@gmail.com>
2018-10-28 16:46:10 +0300

    Increase the limit of protobuf weights files from 64MB to 2GB. (#471)

--- src/neural/loader.cc
+++ src/neural/loader.cc
@@ -26,6 +26,8 @@
 */
 
 #include "neural/loader.h"
+
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
 #include <zlib.h>
 #include <algorithm>
 #include <cctype>
@@ -34,6 +36,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+
 #include "proto/net.pb.h"
 #include "utils/commandline.h"
 #include "utils/exception.h"
@@ -108,8 +111,14 @@ void DenormConvBlock(const pblczero::Weights_ConvBlock& conv,
 
 FloatVectors LoadFloatsFromPbFile(const std::string& buffer) {
   auto net = pblczero::Net();
-  FloatVectors vecs;
-  if (!net.ParseFromString(buffer))
+  using namespace google::protobuf::io;
+
+  ArrayInputStream raw_input_stream(buffer.data(), buffer.size());
+  CodedInputStream input_stream(&raw_input_stream);
+  // Set protobuf limit to 2GB, print warning at 500MB.
+  input_stream.SetTotalBytesLimit(2000 * 1000000, 500 * 1000000);
+
+  if (!net.ParseFromCodedStream(&input_stream))
     throw Exception("Invalid weight file: parse error.");
 
   if (net.magic() != kWeightMagic)
@@ -132,6 +141,7 @@ FloatVectors LoadFloatsFromPbFile(const std::string& buffer) {
 
   const auto& w = net.weights();
 
+  FloatVectors vecs;
   DenormConvBlock(w.input(), &vecs);
 
   for (int i = 0, n = w.residual_size(); i < n; i++) {
