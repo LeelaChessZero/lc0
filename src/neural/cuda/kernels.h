4e0a112ed6dcb249f5f2d9678089116340ffa619
Ankan Banerjee <ankan.ban@gmail.com>
2018-11-20 02:32:16 +0530

    refactor cuda backend (#501)
    - split into more managable parts

---
+++ src/neural/cuda/kernels.h
@@ -0,0 +1,66 @@
+/*
+  This file is part of Leela Chess Zero.
+  Copyright (C) 2018 The LCZero Authors
+
+  Leela Chess is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Leela Chess is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
+
+  Additional permission under GNU GPL version 3 section 7
+
+  If you modify this Program, or any covered work, by linking or
+  combining it with NVIDIA Corporation's libraries from the NVIDIA CUDA
+  Toolkit and the NVIDIA CUDA Deep Neural Network library (or a
+  modified version of those libraries), containing parts covered by the
+  terms of the respective license agreement, the licensors of this
+  Program grant you additional permission to convey the resulting work.
+*/
+
+namespace lczero {
+namespace cudnn_backend {
+
+// Adds two vectors (possibly of different sizes), also do optional
+// activation (relu, tanh or sigmoid)
+template <typename T>
+void addVectors(T* c, T* a, T* b, int size, int asize, int bsize, bool relu,
+                bool use_tanh, bool use_sigmoid);
+
+// add bias to convolution's output
+template <typename T>
+void addBias_NCHW(T* c, T* a, T* b, int N, int C, int H, int W);
+
+// Conversion from: fp32 -> fp16 datatype, and NCHW -> NHWC layout
+// cudnn kernels work best with NCHW layout for fp32, and with NHWC for fp16
+void fp32NCHWtofp16NHWC(half* output_tensor, float* input_tensor, int Nin,
+                        int Cin, int Nout, int Cout, int H, int W);
+
+// plain data-type conversion (no layout conversion)
+template <typename DstType, typename SrcType>
+void copyTypeConverted(DstType* op, SrcType* ip, int N);
+
+// perform batch normilization
+template <typename T>
+void batchNorm(T* output, const T* input, const T* skipInput, int N,
+               int C, int H, int W, float* means, float* var_multipliers,
+               bool relu);
+
+// unpack planes (input to network)
+void expandPlanes_Fp32_NCHW(float* output, const uint64_t* masks,
+                            const float* values, int n);
+
+
+void expandPlanes_Fp16_NHWC(half* output, const uint64_t* masks,
+                            const float* values, int n);
+
+}  // namespace cudnn_backend
+}  // namespace lczero
+
