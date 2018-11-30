8758a4c6703439398a4be7a21e44fdfcb648530d
borg323 <borg323@users.noreply.github.com>
2018-11-30 10:27:43 +0200

    remove cuda_include from build-cuda.cmd

--- build-cuda.cmd
+++ build-cuda.cmd
@@ -5,9 +5,8 @@ set MSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBui
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 meson.py build --backend vs2017 --buildtype release ^
--Dcuda_include="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\include" ^
 -Dcudnn_libdirs="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\lib\x64","C:\dev\cuDNN\cuda\lib\x64" ^
--Dcudnn_include="C:\dev\cuDNN\cuda\include" ^
+-Dcudnn_include="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\include","C:\dev\cuDNN\cuda\include" ^
 -Ddefault_library=static
 
 pause
