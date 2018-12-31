5ce11609a523f26708b8ea65600a7fd5fdf635ba
SunnyWar <itsallthesametome@hotmail.com>
2018-12-30 22:16:32 -0800

    Updated commend

--- src/mcts/node.cc
+++ src/mcts/node.cc
@@ -73,7 +73,7 @@ class NodeGarbageCollector {
     // Lock the mutex and remove all the pointers
     Mutex::Lock lock(gc_mutex_);
     while (!stop_.load() && !subtrees_to_gc_.empty()) {
-      // Node will be released in destructor when mutex is not locked.
+      // Node will be released in destructor when node is removed
       subtrees_to_gc_.pop_back();
     }
   }
