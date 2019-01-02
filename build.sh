544345e1a5e545f157fc6b2fdbfc297d33ad1b74
borg323 <borg323@users.noreply.github.com>
2019-01-02 18:08:08 +0200

    add pgo and lto build types to build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,36 @@
 
 set -e
 
+EXTRA="-Db_pgo=off -Db_lto=false"
+NET=
+
 case $1 in
   plain|debug|debugoptimized|release|minsize)
     BUILDTYPE=$1
+    shift
+    ;;
+  pgo)
+  # Search for the latest network file to use for the pgo run.
+    file -C -m scripts/magic
+    for f in * networks/*
+    do
+      if [ -f "$f" ] && [ "$NET" -ot "$f" ]
+      then
+        zcat -f "$f" | file -m scripts/magic - | grep -q LeelaChessZero && NET="$f"
+      fi
+    done
+    if [ ! -f "$NET" ]
+    then
+      echo No network file found!
+      exit 0
+    fi
+    BUILDTYPE=release
+    EXTRA="-Db_pgo=generate -Db_lto=true"
+    shift
+    ;;
+  lto)
+    BUILDTYPE=release
+    EXTRA="-Db_pgo=off -Db_lto=true"
     shift
     ;;
   *)
@@ -16,14 +43,22 @@ BUILDDIR=build/${BUILDTYPE}
 
 if [ -f ${BUILDDIR}/build.ninja ]
 then
-  meson configure ${BUILDDIR} -Dbuildtype=${BUILDTYPE} -Dprefix=${INSTALL_PREFIX:-/usr/local} "$@"
+  meson configure ${BUILDDIR} -Dbuildtype=${BUILDTYPE} -Dprefix=${INSTALL_PREFIX:-/usr/local} ${EXTRA} "$@"
 else
-  meson ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local} "$@"
+  meson ${BUILDDIR} --buildtype ${BUILDTYPE} --prefix ${INSTALL_PREFIX:-/usr/local} ${EXTRA} "$@"
 fi
 
 pushd ${BUILDDIR}
 
 NINJA=$(awk '/ninja/ {ninja=$4} END {print ninja}' meson-logs/meson-log.txt)
+
+# For a pgo run generate the profile data.
+if [ -f ../../"$NET" ]
+then
+  ${NINJA}
+  ./lc0 benchmark --weights=../../"$NET" --movetime=30000
+  meson configure -Db_pgo=use
+fi
 
 if [ -n "${INSTALL_PREFIX}" ]
 then
