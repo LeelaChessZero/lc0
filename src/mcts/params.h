f819cd4cbec37fdee39ec8a7fd06447679761987
Melle Koning <mellekoning@gmail.com>
2019-03-24 17:02:46 +0100

    Add EdgeDiscardFactor

--- src/mcts/params.h
+++ src/mcts/params.h
@@ -97,6 +97,11 @@ class SearchParams {
   float GetMinimumKLDGainPerNode() const {
     return options_.Get<float>(kMinimumKLDGainPerNode.GetId());
   }
+  float GetEdgeDiscardFactor() const {
+	  return options_.Get<float>(kEdgeDiscardFactor.GetId());
+  }
+
+  
 
   // Search parameter IDs.
   static const OptionId kMiniBatchSizeId;
@@ -129,6 +134,7 @@ class SearchParams {
   static const OptionId kHistoryFillId;
   static const OptionId kMinimumKLDGainPerNode;
   static const OptionId kKLDGainAverageInterval;
+  static const OptionId kEdgeDiscardFactor;
 
  private:
   const OptionsDict& options_;
