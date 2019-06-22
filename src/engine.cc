26d7f0b8271e27be68fbd69e8ede01abbb7bd63a
TripleAsterick <www.zheng813@gmail.com>
2019-06-21 18:00:36 -0700

    fix grammar

--- src/engine.cc
+++ src/engine.cc
@@ -436,7 +436,7 @@ void EngineLoop::CmdIsReady() {
 void EngineLoop::CmdSetOption(const std::string& name, const std::string& value,
                               const std::string& context) {
   options_.SetUciOption(name, value, context);
-  // Set the log filename for the case it was set in UCI option.
+  // Set the log filename in case it was set in UCI option.
   Logging::Get().SetFilename(
       options_.GetOptionsDict().Get<std::string>(kLogFileId.GetId()));
 }
