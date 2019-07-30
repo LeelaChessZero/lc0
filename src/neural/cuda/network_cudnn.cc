9746944aaf359df16216fa4c95e1439983aeccaf
Alexis Olson <AlexisOlson@gmail.com>
2019-07-30 10:04:25 -0500

    Catch up to master (#7)
    * Centipawn conversion (#860)
    
    * Non-tangent Centipawn Conversion 
    
    This conversion formula should be a better match near the asymptotes.
    
    * Added coefficient to the denominator
    
    This coefficient makes the max score top out near +128 (12826 cp, more precisely).
    
    This also fixes the divide-by-zero problem at x = +/- 1.
    
    * Exponent tweak and coefficient precision
    
    The coefficient 0.976953125 = 1 - 295/12800.
    
    * check gtest dependency only if needed (#862)
    
    * blas output cleanup (#863)
    
    *  use Eigen as a BLAS alternative  (#858)
    
    * import Eigen 3.3.7
    
    * fix WinogradConvolution3 allocation size (#864)
    
    * Add pushd command in build.sh (#865)
    
    Hmm I'm not entirely sure this is right.
    
    * Correct allocation size of M matrix in Winograd convolution (#866)
    
    * Revert "fix WinogradConvolution3 allocation size (#864)"
    
    This reverts commit 6a425b51fe904ee55a05cce1dac50ddd9d08012d.
    
    * better fix for WinogradConvolution3 allocation size
    
    * [AppVeyor] Set clone folder (#867)
    
    * Divide by a slightly smaller divisor to truncate to +/-12800. (#880)
    
    * Use the best move eval instead of highest eval for temperature cutoff. (#772)
    
    * Set probabilities for illegal moves in a loop (#885)
    
    * Set probabilities for illegal moves in a loop
    
    Set probabilities for illegal moves in a loop instead of using std::memset.
    
    * Use std::fill instead of a for loop
    
    Use std::fill instead of a for loop during moves probabilities array initialization.
    
    * fix calculation of max size needed to hold tensors (#892)
    
    
    * fix maxSize for tensor buffers
    
     - no of filters can be less than no. of input planes!
    
    * fix calculation for value head allocation too
    
    - need 3x the size for wdl
    
    * take max of all layers when computing size to avoid assumptions
    
    * Check for null parent in search backup. (#887)
    
    * Fix appveyor breakage due to meson fix. (#904)
    
    * change protobuf appveyor build (remove -Dprotobuf_MSVC_STATIC_RUNTIME=NO)
    
    * clear appveyor cache
    
    * set static/shared crt option for nvcc according to default_library and meson version (for fixed bug)
    
    * Eigen as meson subproject (#906)
    
    * Additional reporting details for selfplay to support T50+ (#821)
    
    * Hack to report average nodes per move for monitoring training.
    
    * WDLstyle resign false positive reporting
    
    * Make this less WIP and more submittable.
    
    * Cast correctly.
    
    * Fix typo in last commit...
    
    * Add comment about unsupported combination.

--- src/neural/cuda/network_cudnn.cc
+++ src/neural/cuda/network_cudnn.cc
@@ -77,7 +77,7 @@ void dumpTensor(void *memory, int elements, char *message, bool fp16 = false)
 #endif
 
 struct InputsOutputs {
-  InputsOutputs(int maxBatchSize) {
+  InputsOutputs(int maxBatchSize, bool wdl) {
     ReportCUDAErrors(cudaHostAlloc(
         &input_masks_mem_, maxBatchSize * kInputPlanes * sizeof(uint64_t),
         cudaHostAllocMapped));
@@ -99,7 +99,8 @@ struct InputsOutputs {
     ReportCUDAErrors(cudaMalloc(
         &op_policy_mem_gpu_, maxBatchSize * kNumOutputPolicy * sizeof(float)));
 
-    ReportCUDAErrors(cudaHostAlloc(&op_value_mem_, maxBatchSize * sizeof(float),
+    ReportCUDAErrors(
+        cudaHostAlloc(&op_value_mem_, maxBatchSize * (wdl ? 3 : 1) * sizeof(float),
                                    cudaHostAllocMapped));
     ReportCUDAErrors(
         cudaHostGetDevicePointer(&op_value_mem_gpu_, op_value_mem_, 0));
@@ -437,7 +438,15 @@ class CudnnNetwork : public Network {
     // 3. Allocate GPU memory for running the network:
     //    - three buffers of max size are enough (one to hold input, second to
     //      hold output and third to hold skip connection's input).
-    size_t maxSize = resi_last_->GetOutputSize(max_batch_size_);
+
+    // size of input to the network
+    size_t maxSize = max_batch_size_ * kNumInputPlanes * 64 * sizeof(DataType);
+
+    // take max size of all layers
+    for (auto& layer : network_) {
+      maxSize = std::max(maxSize, layer->GetOutputSize(max_batch_size_));
+    }
+
     for (auto& mem : tensor_mem_) {
       ReportCUDAErrors(cudaMalloc(&mem, maxSize));
       ReportCUDAErrors(cudaMemset(mem, 0, maxSize));
@@ -652,7 +661,7 @@ class CudnnNetwork : public Network {
   std::unique_ptr<InputsOutputs> GetInputsOutputs() {
     std::lock_guard<std::mutex> lock(inputs_outputs_lock_);
     if (free_inputs_outputs_.empty()) {
-      return std::make_unique<InputsOutputs>(max_batch_size_);
+      return std::make_unique<InputsOutputs>(max_batch_size_, wdl_);
     } else {
       std::unique_ptr<InputsOutputs> resource =
           std::move(free_inputs_outputs_.front());
@@ -669,7 +678,7 @@ class CudnnNetwork : public Network {
   // Apparently nvcc doesn't see constructor invocations through make_unique.
   // This function invokes constructor just to please complier and silence
   // warning. Is never called (but compiler thinks that it could).
-  void UglyFunctionToSilenceNvccWarning() { InputsOutputs io(0); }
+  void UglyFunctionToSilenceNvccWarning() { InputsOutputs io(0, false); }
 
  private:
   cudnnHandle_t cudnn_;
