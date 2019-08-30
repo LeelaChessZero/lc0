4b165a65d644b11988bf77ac9206db257e74ffc6
Alexis Olson <AlexisOlson@gmail.com>
2019-08-30 15:37:42 -0500

    D -> d

--- src/mcts/params.cc
+++ src/mcts/params.cc
@@ -49,7 +49,7 @@ const OptionId SearchParams::kMaxPrefetchBatchId{
     "When the engine cannot gather a large enough batch for immediate use, try "
     "to prefetch up to X positions which are likely to be useful soon, and put "
     "them into cache."};
-const OptionId SearchParams::kLogitQEnabledID{
+const OptionId SearchParams::kLogitQEnabledId{
     "logit-q-enabled", "LogitQEnabled",
     "Whether to sum Q+U in logit space or not."};
 const OptionId SearchParams::kCpuctId{
