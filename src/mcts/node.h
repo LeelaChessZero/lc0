c027aa312d5786c237975c56c2b1c5a25cff9398
Alexis Olson <AlexisOlson@gmail.com>
2019-09-20 08:50:04 -0500

    Remove scaling factor

--- src/mcts/node.h
+++ src/mcts/node.h
@@ -340,8 +340,7 @@ class EdgeAndNode {
   float GetQ(float default_q, bool logit_q = false) const {
     return (node_ && node_->GetN() > 0)
                ?
-               // Scale Q slightly to avoid logit(1) = infinity.
-               (logit_q ? FastLogit(0.9999999f * node_->GetQ()) : node_->GetQ())
+               (logit_q ? FastLogit(node_->GetQ()) : node_->GetQ())
                : default_q;
   }
   float GetD() const {
