384589bd025eb61779f6e83f6b63f24053fa724c
Tilps <Tilps@users.noreply.github.com>
2020-01-07 23:11:39 +1100

    Start to fill in skeleton a bit.

--- src/neural/network_external.cc
+++ src/neural/network_external.cc
@@ -31,9 +31,57 @@
 #include "utils/optionsdict.h"
 #include "utils/transpose.h"
 
+#ifndef _WIN32
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#else
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#endif
+
 namespace lczero {
 
 namespace {
+
+void* make_map(std::string file_name, unsigned long long needed_size)
+{
+  void* base_address;
+#ifndef _WIN32
+  struct stat statbuf;
+  // TODO: create file with specific size, not open readonly.
+  int fd = ::open(file_name.c_str(), O_RDONLY);
+  if (fd == -1) return nullptr;
+  base_address = mmap(nullptr, needed_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+  ::close(fd);
+  if (base_address == MAP_FAILED) {
+    exit(1);
+  }
+#else
+  const HANDLE fd =
+      CreateFileA(file_name.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, nullptr,
+                  CREATE_ALWAYS, FILE_ATTRIBUTE_TEMPORARY, nullptr);
+  LARGE_INTEGER file_size;
+  file_size.QuadPart = needed_size;
+  SetFilePointerEx(fd, file_size, nullptr, 0);
+  SetEndOfFile(fd);
+  if (fd == INVALID_HANDLE_VALUE) return nullptr;
+  HANDLE mmap =
+      CreateFileMapping(fd, nullptr, PAGE_READWRITE, needed_size >> 32,
+                                  needed_size & 0xFFFFFFFFu, nullptr);
+  CloseHandle(fd);
+  if (!mmap) {
+    exit(1);
+  }
+  base_address = MapViewOfFile(mmap, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 0);
+  if (!base_address) {
+    exit(1);
+  }
+#endif
+  return base_address;
+}
 
 class ExternalNetworkComputation;
 
@@ -41,11 +89,30 @@ class ExternalNetwork : public Network {
  public:
   ExternalNetwork(const WeightsFile& file, const OptionsDict& options) {
     // Serialize file to bytes.
+    auto data = file.SerializeAsString();
     // Make large memory mapped file big enough to contain plus some extra and
     // also at least max batch size times size of inputs, wdl and policies.
+    auto mmap_name = options.GetOrDefault<std::string>(
+        "mmap_file", "external_net_transport");
+    map_ = make_map(
+        mmap_name,
+        std::max(data.size() + 16, static_cast<size_t>(16) + 1024 * 4 * (116 * 8 * 8 + 1858 + 3)));
+    // TODO: There is a race if external sees file and maps it before we clear this flag and the 'undefined value' happens to be read as a 1.
+    static_cast<size_t*>(map_)[0] = 0;
     // write weights bytes at small offset.
+    memcpy(static_cast<char*>(map_) + 16, data.data(), data.size());
     // Write 'weights ready' flag.
+    static_cast<size_t*>(map_)[0] = 1;
     // Spin Wait for 'dest ready' flag.
+    // TODO: use atomic read, not just volatile to get better semantic guarantee.
+    while (static_cast<volatile size_t*>(map_)[1] != 1) {
+      // Sleep a ms or two should be fine for this loop.  TODO:
+    }
+  }
+
+  ~ExternalNetwork()
+  {
+    // unmap map_ and delete.
   }
 
   std::unique_ptr<NetworkComputation> NewComputation() override;
@@ -56,9 +123,16 @@ class ExternalNetwork : public Network {
     // Take lock.
     // Write raw_input at small offset with length.
     // Write 'input ready' flag value.
+    static_cast<size_t*>(map_)[0] = 2;
     // Otherside clears input ready flag.
     // Spin Wait for 'output ready' flag value.
+    // TODO: use atomic read, not just volatile to get better semantic
+    // guarantee.
+    while (static_cast<volatile size_t*>(map_)[1] != 2) {
+      // TODO: no sleep for first n loops, then sleep 0.
+    }
     // Clear 'output ready' flag. (Maybe atomic_compare_swap in spin wait?)
+    static_cast<size_t*>(map_)[1] = 0;
     // Copy output in wdls/policies.
   }
 
@@ -69,6 +143,7 @@ class ExternalNetwork : public Network {
   }
 
  private:
+  void* map_;
 };
 
 class ExternalNetworkComputation : public NetworkComputation {
