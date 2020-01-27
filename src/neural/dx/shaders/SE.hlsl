7729809a1fac52bb2e5d73823dab18f6e054c732
Ankan <ankan.ban@gmail.com>
2020-01-27 19:48:12 +0530

    update copyright message

--- src/neural/dx/shaders/SE.hlsl
+++ src/neural/dx/shaders/SE.hlsl
@@ -14,6 +14,15 @@
 
   You should have received a copy of the GNU General Public License
   along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
+
+  Additional permission under GNU GPL version 3 section 7
+
+  If you modify this Program, or any covered work, by linking or
+  combining it with NVIDIA Corporation's libraries from the NVIDIA CUDA
+  Toolkit and the NVIDIA CUDA Deep Neural Network library (or a
+  modified version of those libraries), containing parts covered by the
+  terms of the respective license agreement, the licensors of this
+  Program grant you additional permission to convey the resulting work.
 */
 
 cbuffer consts : register(b0) {
