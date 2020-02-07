3ac4d566c913a23897b023d9218fd6f2226704a5
Alexander Lyashuk <crem@google.com>
2020-02-07 21:48:50 +0100

    Typo.

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -131,7 +131,7 @@ void Search::SendUciInfo() REQUIRES(nodes_mutex_) {
     const auto q = edge.GetQ(default_q, GetDrawScore(false));
     if (score_type == "centipawn_with_drawscore") {
       uci_info.score = 295 * q / (1 - 0.976953126 * std::pow(q, 14));
-    } else if (score_type == "centipawn_2019") {
+    } else if (score_type == "centipawn") {
       uci_info.score = 295 * wl / (1 - 0.976953126 * std::pow(q, 14));
     } else if (score_type == "centipawn_2018") {
       uci_info.score = 290.680623072 * tan(1.548090806 * wl);
