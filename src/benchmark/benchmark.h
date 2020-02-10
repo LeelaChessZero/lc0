73ccf18f63635d9479059efc7c826bfdd56e2b8a
cn4750 <can00336@gmail.com>
2020-02-10 16:08:46 -0500

    Fix assignment
    Allow assignment for starting position.

--- src/benchmark/benchmark.h
+++ src/benchmark/benchmark.h
@@ -39,7 +39,7 @@ class Benchmark{
   Benchmark() = default;
 
   // Same positions as Stockfish uses.
-  const std::vector<std::string> positions = {
+  std::vector<std::string> positions = {
       "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
       "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10",
       "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 11",
