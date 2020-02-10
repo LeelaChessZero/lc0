8b9c8ca8d7a73f833921151bb272a054bdcd63df
cn4750 <can00336@gmail.com>
2020-02-10 16:03:12 -0500

    Update the check scripts to use only the starting position in bench
    Bench only the starting position to save time.

--- scripts/check_opencl.bat
+++ scripts/check_opencl.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Sanity checking the opencl driver.
-lc0 benchmark --backend=check --backend-opts=mode=check,freq=1.0,opencl,blas %*
+lc0 benchmark --starting-position --backend=check --backend-opts=mode=check,freq=1.0,opencl,blas %*
 PAUSE
 
