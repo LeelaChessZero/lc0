b80bfe691054c2dd5c0e98194e533967ba4b4d41
cn4750 <can00336@gmail.com>
2020-02-17 19:47:45 -0500

    Remove positions which inflate nps
    Remove TB endgame positions which inflate nps greatly.

--- src/benchmark/benchmark.h
+++ src/benchmark/benchmark.h
@@ -75,16 +75,8 @@ class Benchmark{
       "5rk1/q6p/2p3bR/1pPp1rP1/1P1Pp3/P3B1Q1/1K3P2/R7 w - - 93 90",
       "4rrk1/1p1nq3/p7/2p1P1pp/3P2bp/3Q1Bn1/PPPB4/1K2R1NR w - - 40 21",
       "r3k2r/3nnpbp/q2pp1p1/p7/Pp1PPPP1/4BNN1/1P5P/R2Q1RK1 w kq - 0 16",
-      "3Qb1k1/1r2ppb1/pN1n2q1/Pp1Pp1Pr/4P2p/4BP2/4B1R1/1R5K b - - 11 40",
-      "8/8/8/8/5kp1/P7/8/1K1N4 w - - 0 1",
-      "8/8/8/5N2/8/p7/8/2NK3k w - - 0 1",
-      "8/3k4/8/8/8/4B3/4KB2/2B5 w - - 0 1",
-      "8/8/1P6/5pr1/8/4R3/7k/2K5 w - - 0 1",
-      "8/2p4P/8/kr6/6R1/8/8/1K6 w - - 0 1",
-      "8/8/3P3k/8/1p6/8/1P6/1K3n2 b - - 0 1",
-      "8/R7/2q5/8/6k1/8/1P5p/K6R w - - 0 124",
-      "6k1/3b3r/1p1p4/p1n2p2/1PPNpP1q/P3Q1p1/1R1RB1P1/5K2 b - - 0 1",
-      "r2r1n2/pp2bk2/2p1p2p/3q4/3PN1QP/2P3R1/P4PP1/5RK1 w - - 0 1"};
+      "3Qb1k1/1r2ppb1/pN1n2q1/Pp1Pp1Pr/4P2p/4BP2/4B1R1/1R5K b - - 11 40"
+  };
 
   void Run();
   void OnBestMove(const BestMoveInfo& move);
