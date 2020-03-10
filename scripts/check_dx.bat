63ed3cd6df9fbe0a528338f7f7731e804dd0dd01
cn4750 <can00336@gmail.com>
2020-03-10 09:30:15 -0400

    Update the check scripts to use the new benchmark option
    Use --num-positions=1 instead of --starting-position.

--- scripts/check_dx.bat
+++ scripts/check_dx.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Sanity checking the dx12 driver.
-lc0 benchmark --starting-position --backend=check --backend-opts=mode=check,freq=1.0,atol=5e-1,dx12,blas %*
+lc0 benchmark --num-positions=1 --backend=check --backend-opts=mode=check,freq=1.0,atol=5e-1,dx12,blas %*
 PAUSE
 
