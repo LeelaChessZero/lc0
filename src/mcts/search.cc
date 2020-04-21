8c16bed027e8a765b8b262e6c029de6520ed10ed
Henrik Forstén <henrik.forsten@gmail.com>
2020-04-21 14:41:59 +0300

    Tune constants

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -145,9 +145,9 @@ void Search::SendUciInfo() REQUIRES(nodes_mutex_) {
           std::round(edge.GetM(0.0f)) / 2 + (edge.IsTbTerminal() ? 101 : 1),
           wl);
     } else if (score_type == "centipawn_with_drawscore") {
-      uci_info.score = 111.714640912 * tan(1.5620688421 * q);
+      uci_info.score = 90 * tan(1.5637541897 * q);
     } else if (score_type == "centipawn") {
-      uci_info.score = 111.714640912 * tan(1.5620688421 * wl);
+      uci_info.score = 90 * tan(1.5637541897 * wl);
     } else if (score_type == "centipawn_2019") {
       uci_info.score = 295 * wl / (1 - 0.976953126 * std::pow(wl, 14));
     } else if (score_type == "centipawn_2018") {
