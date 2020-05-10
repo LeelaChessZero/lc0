f1ce36dd8512e77616f75b7ce02760c130f002a4
Naphthalin <georgschroeter@googlemail.com>
2020-05-11 00:04:55 +0200

    changed defaults for april factors to kiudee tuned values

--- src/mcts/params.cc
+++ src/mcts/params.cc
@@ -272,8 +272,8 @@ void SearchParams::Populate(OptionsParser* options) {
   options->Add<IntOption>(kMiniBatchSizeId, 1, 1024) = 256;
   options->Add<IntOption>(kMaxPrefetchBatchId, 0, 1024) = 32;
   options->Add<BoolOption>(kLogitQId) = false;
-  options->Add<FloatOption>(kAprilFactorId, 0.0f, 10.0f) = 1.0f;
-  options->Add<FloatOption>(kAprilFactorParentId, 0.0f, 10.0f) = 0.001f;
+  options->Add<FloatOption>(kAprilFactorId, 0.0f, 10.0f) = 0.024f;
+  options->Add<FloatOption>(kAprilFactorParentId, 0.0f, 10.0f) = 0.000003f;
   options->Add<FloatOption>(kCpuctId, 0.0f, 100.0f) = 2.147f;
   options->Add<FloatOption>(kCpuctAtRootId, 0.0f, 100.0f) = 2.147f;
   options->Add<FloatOption>(kCpuctBaseId, 1.0f, 1000000000.0f) = 18368.0f;
