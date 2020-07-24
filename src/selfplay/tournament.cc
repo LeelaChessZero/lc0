8132d36086ea1a2098fe4d41cae3aae7fbf21bdb
Naphthalin <georgschroeter@googlemail.com>
2020-07-24 12:38:07 +0200

    for selfplay, default of twofolddraws = false.

--- src/selfplay/tournament.cc
+++ src/selfplay/tournament.cc
@@ -131,6 +131,7 @@ void SelfPlayTournament::PopulateOptions(OptionsParser* options) {
   defaults->Set<std::string>(SearchParams::kHistoryFillId, "no");
   defaults->Set<std::string>(NetworkFactory::kBackendId, "multiplexing");
   defaults->Set<bool>(SearchParams::kStickyEndgamesId, false);
+  defaults->Set<bool>(SearchParams::kTwoFoldDrawsId, false);
 }
 
 SelfPlayTournament::SelfPlayTournament(
