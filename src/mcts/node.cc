233fe386e8c7ab0e9886f9b9208ce3eee4589af1
Alexis Olson <AlexisOlson@gmail.com>
2020-09-20 09:31:50 -0500

    Rename p0

--- src/mcts/node.cc
+++ src/mcts/node.cc
@@ -352,9 +352,9 @@ void Node::CancelScoreUpdate(int multivisit) {
   best_child_cached_ = nullptr;
 }
 
-void Node::FinalizeScoreUpdate(float v, float d, float m, int k, float p, float a) {
+void Node::FinalizeScoreUpdate(float v, float d, float m, int k, float p0, float a) {
   // Recompute Q.
-  float p += a * FastLog2(1.0f + n_);
+  float p = p0 + a * FastLog2(1.0f + n_);
   float invp = 1.0f / p;
   wl_ = std::pow(n_ * std::pow(0.5f * wl_ + 0.5f, p) / (n_ + k) +
                  k  * std::pow(0.5f * v   + 0.5f, p) / (n_ + k), invp) * 2 - 1;
