65842d8f3fe3b80fb8ce7b6e09453474a5a5a188
Naphthalin <georgschroeter@googlemail.com>
2020-10-29 17:03:19 +0100

    crude attempt at creating more than 1 child when setting up a position

--- src/mcts/node.cc
+++ src/mcts/node.cc
@@ -194,9 +194,17 @@ std::unique_ptr<Edge[]> Edge::FromMovelist(const MoveList& moves) {
 Node* Node::CreateSingleChildNode(Move move) {
   assert(!edges_);
   assert(!child_);
-  edges_ = Edge::FromMovelist({move});
-  num_edges_ = 1;
-  child_ = std::make_unique<Node>(this, 0);
+  if (child_) {
+    edges_.push_back(Edge::FromMovelist({move})[0]);
+    Node* old_child = child_;
+    child_ = std::make_unique<Node>(this, num_edges_);
+    (*child_)->sibling_ = old_child;
+    num_edges_++;
+  } else {
+    edges_ = Edge::FromMovelist({move});
+    num_edges_ = 1;
+    child_ = std::make_unique<Node>(this, 0);
+  }
   return child_.get();
 }
 
