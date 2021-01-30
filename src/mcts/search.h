d2e03fd51afd936ea33decb95047876abfc9301b
Tilps <Tilps@users.noreply.github.com>
2021-01-31 09:25:24 +1100

    Increase use of workspace. (#1498)
    3% increase in nps on benchmark.
    100% increase in nps in #1 positions.

--- src/mcts/search.h
+++ src/mcts/search.h
@@ -379,6 +379,19 @@ class SearchWorker {
   struct TaskWorkspace {
     std::array<Node::Iterator, 256> cur_iters;
     std::vector<std::unique_ptr<std::array<int, 256>>> vtp_buffer;
+    std::vector<std::unique_ptr<std::array<int, 256>>> visits_to_perform;
+    std::vector<int> vtp_last_filled;
+    std::vector<int> current_path;
+    std::vector<Move> moves_to_path;
+    PositionHistory history;
+    TaskWorkspace() {
+      vtp_buffer.reserve(30);
+      visits_to_perform.reserve(30);
+      vtp_last_filled.reserve(30);
+      current_path.reserve(30);
+      moves_to_path.reserve(30);
+      history.Reserve(30);
+    }
   };
 
   struct PickTask {
