c0f3da882511eb158abd94f02bb2401484cb1cf6
Ofek Shochat <50481015+OfekShochat@users.noreply.github.com>
2021-05-29 18:56:02 +0300

    change illegal pawns exception to out_of_range exception

--- src/chess/board.cc
+++ src/chess/board.cc
@@ -1,22 +1,17 @@
 /*
   This file is part of Leela Chess Zero.
   Copyright (C) 2018-2019 The LCZero Authors
-
   Leela Chess is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
   Leela Chess is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
   along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
-
   Additional permission under GNU GPL version 3 section 7
-
   If you modify this Program, or any covered work, by linking or
   combining it with NVIDIA Corporation's libraries from the NVIDIA CUDA
   Toolkit and the NVIDIA CUDA Deep Neural Network library (or a
@@ -1031,7 +1026,7 @@ void ChessBoard::SetFromFen(std::string fen, int* rule50_ply, int* moves) {
       bishops_.set(row, col);
     } else if (c == 'P' || c == 'p') {
       if (row == 8 || row == 0)
-        throw Exception("pawns at the first/eighth rank are forbidden.");
+        throw std::out_of_range("pawns at the first/eighth rank are forbidden.");
       pawns_.set(row, col);
     } else if (c == 'N' || c == 'n') {
       // Do nothing
