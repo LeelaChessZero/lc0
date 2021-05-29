54400902f3ebe876b04744d56cc99b68b4be0282
Ofek Shochat <50481015+OfekShochat@users.noreply.github.com>
2021-05-29 19:09:26 +0300

    fix my other commits again

--- src/chess/board_test.cc
+++ src/chess/board_test.cc
@@ -62,7 +62,7 @@ TEST(ChessBoard, IllegalFirstRankPawns) {
     EXPECT_EQ(err.what(), std::string("pawns at the first/eighth rank are forbidden."));
   }
 }
-
+  
 TEST(ChessBoard, PseudolegalMovesStartingPos) {
   ChessBoard board;
   board.SetFromFen(ChessBoard::kStartposFen);
@@ -2267,8 +2267,15 @@ void TestInvalid(std::string fen) {
     SUCCEED();
   }
 }
-}  // namespace
 
+// Default promotion to knight was leaving an en-passant flag set.
+TEST(ChessBoard, InvalidEnPassantFromKnightPromotion) {
+  ChessBoard board;
+  board.SetFromFen("Q3b3/2P2pnk/3R3p/p7/1pp1p3/PnP1P2P/2B2PP1/5RK1 w - - 1 31");
+  board.ApplyMove(Move("c7c8"));
+  EXPECT_TRUE(board.en_passant().empty());
+}
+}  // namespace
 
 TEST(ChessBoard, InvalidFEN) {
   TestInvalid("rnbqkbnr/ppppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
