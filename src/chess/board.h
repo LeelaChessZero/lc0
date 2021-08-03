b00a047f16d64f6336d70270129971d4eba97dcb
borg323 <borg323@users.noreply.github.com>
2021-08-04 01:04:32 +0300

    resolve merge conflict

--- src/chess/board.h
+++ src/chess/board.h
@@ -136,6 +136,9 @@ class ChessBoard {
 
     void Mirror() { data_ = ((data_ & 0b11) << 2) + ((data_ & 0b1100) >> 2); }
 
+    // Note: this is not a strict xfen compatible output. Without access to the
+    // board its not possible to know whether there is ambiguity so all cases
+    // with any non-standard rook positions are encoded in the x-fen format
     std::string as_string() const {
       if (data_ == 0) return "-";
       std::string result;
@@ -164,26 +167,6 @@ class ChessBoard {
       result += 'a' + queenside_rook();
       result += 'a' + kingside_rook();
       result += ']';
-      return result;
-    }
-
-    // Note: this is not a strict xfen compatible output. Without access to the
-    // board its not possible to know whether there is ambiguity so all cases
-    // with any non-standard rook positions are encoded in the x-fen format
-    std::string as_string() const {
-      std::string result;
-      if (data_ == 0) result = "-";
-      if (queenside_rook() == 0 && kingside_rook() == 7) {
-        if (we_can_00()) result += 'K';
-        if (we_can_000()) result += 'Q';
-        if (they_can_00()) result += 'k';
-        if (they_can_000()) result += 'q';
-      } else {
-        if (we_can_00()) result += 'A' + kingside_rook();
-        if (we_can_000()) result += 'A' + queenside_rook();
-        if (they_can_00()) result += 'a' + kingside_rook();
-        if (they_can_000()) result += 'a' + queenside_rook();        
-      }
       return result;
     }
 
