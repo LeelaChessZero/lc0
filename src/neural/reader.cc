19424ac5231d8bf33f0fbccd892fc1f59f438ab6
Tilps <Tilps@users.noreply.github.com>
2021-09-06 14:31:03 +1000

    Upstream a rescorer patch that was missed in the big import.

--- src/neural/reader.cc
+++ src/neural/reader.cc
@@ -187,7 +187,13 @@ bool TrainingDataReader::ReadChunk(V6TrainingData* data) {
           if (read_size != v4_extra + v5_extra) return false;
         }
         data->version = 6;
-        data->result_q = static_cast<float>(data->dummy);
+        // Type of dummy was changed from signed to unsigned - which means -1 on
+        // disk is read in as 255.
+        if (data->dummy > 1 && data->dummy < 255) {
+          throw Exception("Invalid result read in v5 data before upgrade.");
+        }
+        data->result_q =
+            data->dummy == 255 ? -1.0f : (data->dummy == 0 ? 0.0f : 1.0f);
         data->result_d = data->dummy == 0 ? 1.0f : 0.0f;
         data->played_q = 0.0f;
         data->played_d = 0.0f;
