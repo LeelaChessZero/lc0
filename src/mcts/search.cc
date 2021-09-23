72da9ee41295fe07257b84141250bc57c4ea337c
Hans Ekbrand <hans@gpu-master>
2021-09-23 13:27:14 +0200

    don't recaculate the beta prior

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -638,7 +638,7 @@ std::vector<EdgeAndNode> Search::GetBestChildrenNoTemperature(Node* parent,
   if (parent->GetN() == 0) return {};
   const bool is_odd_depth = (depth % 2) == 1;
   const float draw_score = GetDrawScore(is_odd_depth);
-  const int parent_n = parent->GetN();
+  const float beta_prior = pow(parent->GetN(), 0.3);
   // Best child is selected using the following criteria:
   // * Prefer shorter terminal wins / avoid shorter terminal losses.
   // * Largest number of playouts.
@@ -659,7 +659,7 @@ std::vector<EdgeAndNode> Search::GetBestChildrenNoTemperature(Node* parent,
                           : edges.end();
   std::partial_sort(
       edges.begin(), middle, edges.end(),
-      [draw_score, parent_n](const auto& a, const auto& b) {
+      [draw_score, beta_prior](const auto& a, const auto& b) {
         // The function returns "true" when a is preferred to b.
 
         // Lists edge types from less desirable to more desirable.
@@ -705,7 +705,7 @@ std::vector<EdgeAndNode> Search::GetBestChildrenNoTemperature(Node* parent,
         // Neither is terminal, use standard rule.
         if (a_rank == kNonTerminal) {
 
-	  float beta_prior = pow(parent_n, 0.3);
+	  // the beta_prior is constant and equals pow(parent->GetN(), 0.3);
 	  float alpha_prior = 1.0f;
 
 	  float winrate_a = (a.GetQ(0.0f, draw_score) + 1) * 0.5;
