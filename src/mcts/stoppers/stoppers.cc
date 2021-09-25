4c96da9a258f4d4eaac0d2e2db0161414ec6fce0
Hans Ekbrand <hans@gpu-master>
2021-09-25 14:07:13 +0200

    fix coding errors

--- src/mcts/stoppers/stoppers.cc
+++ src/mcts/stoppers/stoppers.cc
@@ -263,11 +263,11 @@ bool SmartPruningStopper::ShouldStop(const IterationStats& stats,
     // Reject early stop if Q and N disagrees
     if(index_of_largest_n != index_of_highest_q){
       LOGFILE << "Rejected smart pruning since child with largest n: " <<
-	index_of_largest_n << ", which has " my_largest_n " (should be same as " << largest_n << ") has lower Q (" << stats.q[index_of_largest_n] << ") than child: " << index_of_highest_q << " which has Q=" << stats.q[index_of_largest_q] << " and has " << stats.edge_n[i] << " visits.";
+	index_of_largest_n << ", which has " << my_largest_n << " (should be same as " << largest_n << ") has lower Q (" << stats.q[index_of_largest_n] << ") than child: " << index_of_highest_q << " which has Q=" << stats.q[index_of_highest_q] << " and has " << stats.edge_n[index_of_highest_q] << " visits.";
       return false;
     } else {
       LOGFILE << "Accepted smart pruning since child with largest n: " <<
-	index_of_largest_n << ", which has " my_largest_n " (should be same as " << largest_n << ") has higest Q (" << stats.q[index_of_largest_n];
+	index_of_largest_n << ", which has " << my_largest_n << " (should be same as " << largest_n << ") has higest Q (" << stats.q[index_of_largest_n];
     }
 
     LOGFILE << remaining_playouts << " playouts remaining. Best move has "
