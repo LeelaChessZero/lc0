20ea0c37833a7404d80f21b915087f22a162abcd
Tilps <Tilps@users.noreply.github.com>
2022-02-27 10:39:31 +1100

    Autoformat network_legacy.cc

--- src/neural/network_legacy.cc
+++ src/neural/network_legacy.cc
@@ -143,7 +143,8 @@ LegacyWeights::FFN::FFN(const pblczero::Weights::FFN& ffn)
       dense2_w(LayerAdapter(ffn.dense2_w()).as_vector()),
       dense2_b(LayerAdapter(ffn.dense2_b()).as_vector()) {}
 
-LegacyWeights::EncoderLayer::EncoderLayer(const pblczero::Weights::EncoderLayer& encoder)
+LegacyWeights::EncoderLayer::EncoderLayer(
+    const pblczero::Weights::EncoderLayer& encoder)
     : mha(MHA(encoder.mha())),
       ln1_gammas(LayerAdapter(encoder.ln1_gammas()).as_vector()),
       ln1_betas(LayerAdapter(encoder.ln1_betas()).as_vector()),
