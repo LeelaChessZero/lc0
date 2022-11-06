de0d65ba309ea4be472aa45c8de0e96e65bdf7fd
Aniebiet Udoh <almaudoh@yahoo.com>
2022-11-06 22:56:18 +0100

    Fix warnings, add exception class, only allocate when encoder layers exist.

--- src/neural/blas/encoder.h
+++ src/neural/blas/encoder.h
@@ -18,11 +18,12 @@
 
 #pragma once
 
-#include "neural/shared/activation.h"
-
 #include <Eigen/Core>
-#include <cstddef>
 #include <cmath>
+#include <cstddef>
+
+#include "neural/shared/activation.h"
+#include "utils/exception.h"
 
 namespace lczero {
 
@@ -36,12 +37,12 @@ template <typename T>
 using ConstEigenMatrixMap =
     Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>;
 
-}
+}  // namespace
 
-void LayerNorm2DWithSkipConnection(const size_t batch_size, const size_t channels,
-                                   float* data, const float* skip, const float* gammas,
-                                   const float* betas, float epsilon)
-{
+void LayerNorm2DWithSkipConnection(const size_t batch_size,
+                                   const size_t channels, float* data,
+                                   const float* skip, const float* gammas,
+                                   const float* betas, float epsilon) {
   for (size_t i = 0; i < batch_size; i++) {
     // Mean taken in dimension C.
     float mean = 0;
@@ -50,7 +51,7 @@ void LayerNorm2DWithSkipConnection(const size_t batch_size, const size_t channel
       mean += data[i * channels + c];
     }
     mean /= channels;
-    
+
     // Variance.
     float var = 0;
     for (size_t c = 0; c < channels; ++c) {
@@ -61,22 +62,18 @@ void LayerNorm2DWithSkipConnection(const size_t batch_size, const size_t channel
 
     // Norm.
     for (size_t c = 0; c < channels; ++c) {
-      data[i * channels + c] = betas[c] + gammas[c]
-        * (data[i * channels + c] - mean) / std::sqrt(var + epsilon);
+      data[i * channels + c] = betas[c] + gammas[c] *
+                                              (data[i * channels + c] - mean) /
+                                              std::sqrt(var + epsilon);
     }
-
   }
-
 }
 
 template <bool use_eigen>
 void AttentionMatmul2D(const bool transpose_a, const bool transpose_b,
-                     const size_t batch_size, const size_t M,
-                     const size_t N, const size_t K,
-                     const float scaling,
-                     const float* input1, const float* input2,
-                     float* output)
-{
+                       const size_t batch_size, const size_t M, const size_t N,
+                       const size_t K, const float scaling, const float* input1,
+                       const float* input2, float* output) {
   for (auto batch = size_t{0}; batch < batch_size; batch++) {
     const float* A = &input1[batch * M * K];
     const float* B = &input2[batch * N * K];
@@ -85,31 +82,25 @@ void AttentionMatmul2D(const bool transpose_a, const bool transpose_b,
       auto C_mat = EigenMatrixMap<float>(C, N, M);
 
       if (transpose_a && transpose_b) {
-        C_mat.noalias() = scaling
-          * ConstEigenMatrixMap<float>(B, K, N).transpose()
-          * ConstEigenMatrixMap<float>(A, M, K).transpose();
-      }
-      else if (transpose_a) {
-        C_mat.noalias() = scaling
-          * ConstEigenMatrixMap<float>(B, N, K)
-          * ConstEigenMatrixMap<float>(A, M, K).transpose();
-      }
-      else if (transpose_b) {
-        C_mat.noalias() = scaling
-          * ConstEigenMatrixMap<float>(B, K, N).transpose()
-          * ConstEigenMatrixMap<float>(A, K, M);
-      }
-      else {
-        C_mat.noalias() = scaling
-          * ConstEigenMatrixMap<float>(B, N, K)
-          * ConstEigenMatrixMap<float>(A, K, M);
+        C_mat.noalias() = scaling *
+                          ConstEigenMatrixMap<float>(B, K, N).transpose() *
+                          ConstEigenMatrixMap<float>(A, M, K).transpose();
+      } else if (transpose_a) {
+        C_mat.noalias() = scaling * ConstEigenMatrixMap<float>(B, N, K) *
+                          ConstEigenMatrixMap<float>(A, M, K).transpose();
+      } else if (transpose_b) {
+        C_mat.noalias() = scaling *
+                          ConstEigenMatrixMap<float>(B, K, N).transpose() *
+                          ConstEigenMatrixMap<float>(A, K, M);
+      } else {
+        C_mat.noalias() = scaling * ConstEigenMatrixMap<float>(B, N, K) *
+                          ConstEigenMatrixMap<float>(A, K, M);
       }
     } else {
 #ifdef USE_BLAS
       cblas_sgemm(CblasRowMajor, transpose_a ? CblasTrans : CblasNoTrans,
-                  transpose_b ? CblasTrans : CblasNoTrans,
-                  M, N, K, scaling, A, transpose_a ? M : K,
-                  B, transpose_b ? K : N, 0.0f, C, N);
+                  transpose_b ? CblasTrans : CblasNoTrans, M, N, K, scaling, A,
+                  transpose_a ? M : K, B, transpose_b ? K : N, 0.0f, C, N);
 #else
       // Should never get here.
       throw Exception("Blas backend internal error");
