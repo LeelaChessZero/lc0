14c868e27bcc144ce7690abe714ebb088678cf90
masterkni6 <masterkni666@gmail.com>
2023-01-20 22:09:46 -0800

    new default to 2022\Community is in Program Files

--- build.cmd
+++ build.cmd
@@ -40,7 +40,7 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019" (
   set backend=vs2017
 ) else (
   where /q cl
-  if errorlevel 1 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+  if errorlevel 1 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
   set backend=vs2022
 )
 
