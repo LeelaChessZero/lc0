a66a7ab37b73544b2c93b06da31fb996d8284908
masterkni6 <masterkni666@gmail.com>
2023-01-20 22:41:17 -0800

    order from latest VS to oldest

--- build.cmd
+++ build.cmd
@@ -30,18 +30,18 @@ set CXX=cl
 set CC_LD=link
 set CXX_LD=link
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019" (
-  where /q cl
-  if errorlevel 1 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-  set backend=vs2019
-) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017" (
-  where /q cl
-  if errorlevel 1 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-  set backend=vs2017
-) else (
+if exist "C:\Program Files\Microsoft Visual Studio\2022" (
   where /q cl
   if errorlevel 1 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
   set backend=vs2022
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019" (
+  where /q cl
+  if errorlevel 1 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+  set backend=vs2019
+) else (
+  where /q cl
+  if errorlevel 1 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+  set backend=vs2017
 )
 
 set BLAS=true
