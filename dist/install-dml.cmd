d6547c7fc7049851e37d2dbb8f7ce968e4ee72eb
borg323 <borg323@users.noreply.github.com>
2023-01-22 13:58:42 +0200

    handle running from different directory or context menu

--- dist/install-dml.cmd
+++ dist/install-dml.cmd
@@ -2,6 +2,15 @@
 where /q tar
 if errorlevel 1 goto error
 
+where /q lc0.exe
+if errorlevel 1 cd /d %~dp0
+where /q lc0.exe
+if errorlevel 1 (
+  echo This script must run in the lc0 folder.
+  pause
+  exit /b
+)
+
 cls
 echo Installing the DirectML.dll version required by the Lc0 onnx-dml backend.
 curl -# --ssl-no-revoke -o tmp_directml.zip https://globalcdn.nuget.org/packages/microsoft.ai.directml.1.10.0.nupkg"
@@ -19,5 +28,4 @@ exit /b
 cls
 echo Installation failed - see the README for an alternative approach.
 pause
-
 
