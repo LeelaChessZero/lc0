9e9a5762281f2eea5ae793ef581ae1d0ea8bb799
borg323 <39573933+borg323@users.noreply.github.com>
2023-02-12 23:40:49 +0200

    support onnx opset 18 (#1845)

--- src/neural/onnx/builder.h
+++ src/neural/onnx/builder.h
@@ -107,6 +107,7 @@ class OnnxBuilder {
                                  const std::string& input,
                                  const OnnxConst& scale, const OnnxConst& bias,
                                  int axis, float epsilon = 1e-6);
+  std::string Mish(const std::string& name, const std::string& input);
   // Returns ONNX model as protobuf.
   const pblczero::ModelProto& as_proto() const { return model_; }
   // Returns serialized model.
