53b31aec30c069155749fd788e0fb68a925957cf
Naphthalin <40385638+Naphthalin@users.noreply.github.com>
2023-05-28 12:20:08 +0200

    WDL Conversion for more realistic WDL and contempt (#1791)
    Adds an Elo based WDL transformation of the NN value head output. Helps with more accurate play at high level (WDL sharpening), more aggressive play against weaker opponents and draw avoiding openings (contempt), piece odds play.
    
    Also adds a new ScoreType `WDL_mu` which follows the new eval convention, where +1.00 means 50% white win chance.

--- src/mcts/params.h
+++ src/mcts/params.h
@@ -33,10 +33,22 @@
 
 namespace lczero {
 
+enum class ContemptPerspective { STM, WHITE, BLACK, NONE };
+
 class SearchParams {
  public:
   SearchParams(const OptionsDict& options);
   SearchParams(const SearchParams&) = delete;
+
+  // Use struct for WDLRescaleParams calculation to make them const.
+  struct WDLRescaleParams {
+    WDLRescaleParams(float r, float d) {
+      ratio = r;
+      diff = d;
+    }
+    float ratio;
+    float diff;
+  };
 
   // Populates UciOptions with search parameters.
   static void Populate(OptionsParser* options);
@@ -104,10 +116,16 @@ class SearchParams {
   }
   bool GetDisplayCacheUsage() const { return kDisplayCacheUsage; }
   int GetMaxConcurrentSearchers() const { return kMaxConcurrentSearchers; }
+  ContemptPerspective GetContemptPerspective() const {
+    return kContemptPerspective;
+  }
   float GetSidetomoveDrawScore() const { return kDrawScoreSidetomove; }
   float GetOpponentDrawScore() const { return kDrawScoreOpponent; }
   float GetWhiteDrawDelta() const { return kDrawScoreWhite; }
   float GetBlackDrawDelta() const { return kDrawScoreBlack; }
+  float GetWDLRescaleRatio() const { return kWDLRescaleParams.ratio; }
+  float GetWDLRescaleDiff() const { return kWDLRescaleParams.diff; }
+  float GetWDLEvalObjectivity() const { return kWDLEvalObjectivity; }
   int GetMaxOutOfOrderEvals() const { return kMaxOutOfOrderEvals; }
   float GetNpsLimit() const { return kNpsLimit; }
   int GetSolidTreeThreshold() const { return kSolidTreeThreshold; }
@@ -184,10 +202,19 @@ class SearchParams {
   static const OptionId kMovesLeftSlopeId;
   static const OptionId kDisplayCacheUsageId;
   static const OptionId kMaxConcurrentSearchersId;
+  static const OptionId kContemptPerspectiveId;
   static const OptionId kDrawScoreSidetomoveId;
   static const OptionId kDrawScoreOpponentId;
   static const OptionId kDrawScoreWhiteId;
   static const OptionId kDrawScoreBlackId;
+  static const OptionId kContemptId;
+  static const OptionId kContemptMaxValueId;
+  static const OptionId kWDLCalibrationEloId;
+  static const OptionId kWDLContemptAttenuationId;
+  static const OptionId kWDLEvalObjectivityId;
+  static const OptionId kWDLDrawRateTargetId;
+  static const OptionId kWDLDrawRateReferenceId;
+  static const OptionId kWDLBookExitBiasId;
   static const OptionId kMaxOutOfOrderEvalsId;
   static const OptionId kNpsLimitId;
   static const OptionId kSolidTreeThresholdId;
@@ -201,6 +228,8 @@ class SearchParams {
   static const OptionId kMaxCollisionVisitsScalingStartId;
   static const OptionId kMaxCollisionVisitsScalingEndId;
   static const OptionId kMaxCollisionVisitsScalingPowerId;
+  static const OptionId kUCIOpponentId;
+  static const OptionId kUCIRatingAdvId;
 
  private:
   const OptionsDict& options_;
@@ -244,6 +273,10 @@ class SearchParams {
   const float kDrawScoreOpponent;
   const float kDrawScoreWhite;
   const float kDrawScoreBlack;
+  const ContemptPerspective kContemptPerspective;
+  const float kContempt;
+  const WDLRescaleParams kWDLRescaleParams;
+  const float kWDLEvalObjectivity;
   const int kMaxOutOfOrderEvals;
   const float kNpsLimit;
   const int kSolidTreeThreshold;
