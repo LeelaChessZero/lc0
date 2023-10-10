63a7cb29e6d7a1680889d81c8342c16e8f5ab66e
Mcgrievy, Kathleen <kathleen.mcgrievy@intel.com>
2023-10-10 16:46:36 -0700

    Updated license for sycl code.

--- src/neural/sycl/inputs_outputs.h
+++ src/neural/sycl/inputs_outputs.h
@@ -25,6 +25,25 @@
   Program grant you additional permission to convey the resulting work.
 */
 
+/*   This file is part of Leela Chess Zero.
+    Modifications Copyright (C) 2023 Intel Corporation
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+   
+   SPDX-License-Identifier: GNU General Public License v3.0 only
+*/
+
 #include <sycl/sycl.hpp>
 #include "neural/network.h"
 #include "cuBlasContext.h"
