7cf716766a27a2ed2ffbddaf33efb3f21b28b4c6
Pan Deng <pan.deng@intel.com>
2023-12-09 08:06:10 +0800

    Skip proactively wake up TaskWorkers for cpu backend
    In the moment that CPU resource is constraint, potentially, proactively
    wake up TaskWorkers could make SearchWorker off CPU and TaskWorkers
    on CPU, unfortunately there is nothing in picking_tasks_, so that
    TaskWorkers will be busy waiting but actually starving. Even if
    they yield, TaskWorkers are probably picked up by OS scheduler than
    SearchWorker since they slept for a long time.
    
    One observation of above is sometimes we hit bad case: many implicit
    unlock() of the "Mutex::Lock lock(picking_tasks_mutex_)" lasts several
    milliseconds, SearchWorker is off cpu while TaskWorkers are busy using CPU
    spin and yield during that time. Since they are in critical region(protected
    by pending_searchers_), overall performance is slowed down. 60%/78% drops
    is observed for --threads=16 and --threads=60 compare with normal case. With
    this change, the bad case is avoided, even 9%/27% better than normal case.
    
    In the moment that CPU resource is not constraint, since there is thread
    notification when a Node is placed in picking_tasks_, the TaskWorker
    should be waked up soon after that. Per evaluation with configuration
    --threads=16 and --threads=60, this change doesn't introduce regression.

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -1515,8 +1515,19 @@ int SearchWorker::WaitForTasks() {
   }
 }
 
+void SearchWorker::ProactivelyNotifyTaskWorkers() {
+  if (task_workers_ <= 0 || search_->network_->IsCpu())
+    return;
+
+  // While nothing is ready yet - wake the task runners so they are ready to
+  // receive quickly.
+  Mutex::Lock lock(picking_tasks_mutex_);
+  task_added_.notify_all();
+}
+
 void SearchWorker::PickNodesToExtend(int collision_limit) {
   ResetTasks();
+  ProactivelyNotifyTaskWorkers();
   std::vector<Move> empty_movelist;
   // This lock must be held until after the task_completed_ wait succeeds below.
   // Since the tasks perform work which assumes they have the lock, even though
