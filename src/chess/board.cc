c751b4ae9bb8bb0e88afa072604cdb0e9cd1119b
borg323 <borg323@users.noreply.github.com>
2024-01-13 18:55:54 +0200

    fix rook positions after clearing board

--- src/chess/board.cc
+++ src/chess/board.cc
@@ -51,6 +51,7 @@ const BitBoard ChessBoard::kPawnMask = 0x00FFFFFFFFFFFF00ULL;
 
 void ChessBoard::Clear() {
   std::memset(reinterpret_cast<void*>(this), 0, sizeof(ChessBoard));
+  castlings_.SetRookPositions(FILE_A, FILE_H, FILE_A, FILE_H);
 }
 
 void ChessBoard::Mirror() {
