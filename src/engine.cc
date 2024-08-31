c4385a03d1331d396df415f330cf17e6b2fc5ec5
Karl Kfoury <76066593+KarlKfoury@users.noreply.github.com>
2024-08-31 22:28:14 +0300

    logging message notifying of successful setoption operation if completed successfuly
    adresses issue #1598

--- src/engine.cc
+++ src/engine.cc
@@ -444,8 +444,11 @@ void EngineLoop::CmdIsReady() {
 
 void EngineLoop::CmdSetOption(const std::string& name, const std::string& value,
                               const std::string& context) {
-  options_.SetUciOption(name, value, context);
+  bool success = options_.SetUciOption(name, value, context);
   // Set the log filename for the case it was set in UCI option.
+  if (success) { 
+  SendResponse(name + "successfully set to " + value);
+}
   Logging::Get().SetFilename(
       options_.GetOptionsDict().Get<std::string>(kLogFileId));
 }
