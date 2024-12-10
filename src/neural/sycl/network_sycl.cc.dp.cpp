e4f8ef58870d5910c27b5c45068545c2d5530d94
borg323 <borg323@users.noreply.github.com>
2024-12-11 00:35:46 +0200

    clean up license text

--- src/neural/sycl/network_sycl.cc.dp.cpp
+++ src/neural/sycl/network_sycl.cc.dp.cpp
@@ -1,50 +1,23 @@
 /*
   This file is part of Leela Chess Zero.
-  Copyright (C) 2018-2022 The LCZero Authors
+  Copyright (C) 2018-2024 The LCZero Authors
+  Copyright (C) 2023 Intel Corporation
 
-  Leela Chess is free software: you can redistribute it and/or modify
+  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Leela Chess is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
-
-  Additional permission under GNU GPL version 3 section 7
-
-  If you modify this Program, or any covered work, by linking or
-  combining it with NVIDIA Corporation's libraries from the NVIDIA CUDA
-  Toolkit and the NVIDIA CUDA Deep Neural Network library (or a
-  modified version of those libraries), containing parts covered by the
-  terms of the respective license agreement, the licensors of this
-  Program grant you additional permission to convey the resulting work.
-*/
-
-
-/*   This file is part of Leela Chess Zero.
-    Modifications Copyright (C) 2023 Intel Corporation
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
    
-   SPDX-License-Identifier:GNU General Public License v3.0 or later
+  SPDX-License-Identifier:GNU General Public License v3.0 or later
 */
-
 
 #define DPCT_COMPAT_RT_VERSION 12020
 
