ec4b286067deb7417bd648026c0a6a6b57e8c3cb
Alexander Lyashuk <crem@google.com>
2024-12-20 12:32:26 +0100

    Addressing the review comment.

--- src/utils/mutex.h
+++ src/utils/mutex.h
@@ -125,15 +125,12 @@ class CAPABILITY("mutex") SharedMutex {
 };
 
 static inline void SpinloopPause() {
-#if defined(__x86_64__)
+#if defined(__x86_64__) || defined(_M_X64)
   _mm_pause();
-#else
-// Fallback for non-x86_64 architectures - prevents optimization
-#ifdef _MSC_VER
+#elif defined(_MSC_VER)
   __asm {}
 #else
   asm volatile("");
-#endif
 #endif
 }
 
