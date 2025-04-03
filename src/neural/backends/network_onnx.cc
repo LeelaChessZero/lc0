f1d7e3be1d02cb8b74c018c87a79664ed5b5ce4a
borg323 <borg323@users.noreply.github.com>
2025-04-03 17:54:08 +0300

    Revert "make a differentr engine file per gpu"
    This reverts commit f703811775d5a4a1e89731c0bb61ed4e07e6d2b3.

--- src/neural/backends/network_onnx.cc
+++ src/neural/backends/network_onnx.cc
@@ -330,8 +330,7 @@ Ort::SessionOptions OnnxNetwork::GetOptions(int gpu, int threads,
       trt_options["trt_min_subgraph_size"] = "1";
       trt_options["trt_engine_cache_enable"] = "1";
       trt_options["trt_engine_cache_prefix"] =
-          "Lc0_ONNX_TRT_batch_" + std::to_string(batch_size) + "_gpu_" +
-          std::to_string(gpu) + "_";
+          "Lc0_ONNX_TRT_batch_" + std::to_string(batch_size) + "_";
       trt_options["trt_timing_cache_enable"] = "1";
       trt_options["trt_layer_norm_fp32_fallback"] = "1";
       trt_options["trt_force_sequential_engine_build"] = "1";
