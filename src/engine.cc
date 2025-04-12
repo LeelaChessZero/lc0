58ab5341def29163b92f733565d30f0bb89340cf
Alexander Lyashuk <crem@google.com>
2025-04-12 12:40:21 +0200

    Support for ponder in Engine (#2150)

--- src/engine.cc
+++ src/engine.cc
@@ -29,7 +29,6 @@
 
 #include <algorithm>
 
-#include "chess/gamestate.h"
 #include "chess/position.h"
 #include "neural/backend.h"
 #include "neural/memcache.h"
@@ -44,22 +43,25 @@ const OptionId kSyzygyTablebaseId{
     "List of Syzygy tablebase directories, list entries separated by system "
     "separator (\";\" for Windows, \":\" for Linux).",
     's'};
+const OptionId kStrictUciTiming{"strict-uci-timing", "StrictTiming",
+                                "The UCI host compensates for lag, waits for "
+                                "the 'readyok' reply before sending 'go' and "
+                                "only then starts timing."};
+const OptionId kPonderId{
+    "", "Ponder",
+    "Indicates to the engine that it will be requested to ponder. This "
+    "postpones resetting the search tree until the search is started."};
+
 const OptionId kPreload{"preload", "",
                         "Initialize backend and load net on engine startup."};
 }  // namespace
 
 void Engine::PopulateOptions(OptionsParser* options) {
+  options->Add<BoolOption>(kPonderId) = false;
   options->Add<StringOption>(kSyzygyTablebaseId);
+  options->Add<BoolOption>(kStrictUciTiming) = false;
+  options->HideOption(kStrictUciTiming);
   options->Add<BoolOption>(kPreload) = false;
-}
-
-Engine::Engine(const SearchFactory& factory, const OptionsDict& opts)
-    : options_(opts),
-      search_(factory.CreateSearch(&uci_forwarder_, &options_)) {
-  if (options_.Get<bool>(kPreload)) {
-    UpdateBackendConfig();
-    // EnsureSyzygyTablebasesLoaded();
-  }
 }
 
 namespace {
@@ -78,6 +80,74 @@ GameState MakeGameState(const std::string& fen,
   return state;
 }
 }  // namespace
+
+class Engine::UciPonderForwarder : public UciResponder {
+ public:
+  UciPonderForwarder(Engine* engine) : engine_(engine) {}
+
+  void OutputBestMove(BestMoveInfo* info) override {
+    if (!wrapped_) return;
+    wrapped_->OutputBestMove(info);
+  }
+  void OutputThinkingInfo(std::vector<ThinkingInfo>* infos) override {
+    if (!wrapped_) return;
+    if (engine_->last_go_params_ && engine_->last_go_params_->ponder) {
+      assert(engine_->last_position_ &&
+             !engine_->last_position_->moves.empty());
+      const Move ponder_move_ = engine_->last_position_->moves.back();
+      // Output all stats from main variation (not necessary the ponder move)
+      // but PV only from ponder move.
+      ThinkingInfo ponder_info;
+      for (const auto& info : *infos) {
+        if (info.multipv <= 1) {
+          ponder_info = info;
+          if (ponder_info.mate) ponder_info.mate = -*ponder_info.mate;
+          if (ponder_info.score) ponder_info.score = -*ponder_info.score;
+          if (ponder_info.depth > 1) ponder_info.depth--;
+          if (ponder_info.seldepth > 1) ponder_info.seldepth--;
+          if (ponder_info.wdl)
+            std::swap(ponder_info.wdl->w, ponder_info.wdl->l);
+          ponder_info.pv.clear();
+        }
+        if (!info.pv.empty() && info.pv[0] == ponder_move_) {
+          ponder_info.pv.assign(info.pv.begin() + 1, info.pv.end());
+        }
+      }
+      infos->clear();
+      infos->push_back(ponder_info);
+    }
+    wrapped_->OutputThinkingInfo(infos);
+  }
+
+  void Register(UciResponder* wrapped) {
+    if (wrapped_) {
+      throw Exception("UciPonderForwarder already has a wrapped responder");
+    }
+    wrapped_ = wrapped;
+  }
+  void Unregister(UciResponder* wrapped) {
+    if (wrapped_ != wrapped) {
+      throw Exception("UciPonderForwarder doesn't have this wrapped responder");
+    }
+    wrapped_ = nullptr;
+  }
+
+ private:
+  UciResponder* wrapped_ = nullptr;
+  Engine* const engine_;
+};
+
+Engine::Engine(const SearchFactory& factory, const OptionsDict& opts)
+    : uci_forwarder_(std::make_unique<UciPonderForwarder>(this)),
+      options_(opts),
+      search_(factory.CreateSearch(uci_forwarder_.get(), &options_)) {
+  if (options_.Get<bool>(kPreload)) {
+    UpdateBackendConfig();
+    EnsureSyzygyTablebasesLoaded();
+  }
+}
+
+Engine::~Engine() = default;
 
 void Engine::EnsureSearchStopped() {
   search_->AbortSearch();
@@ -119,13 +189,33 @@ void Engine::EnsureSyzygyTablebasesLoaded() {
   search_->SetSyzygyTablebase(syzygy_tb_.get());
 }
 
+// Initializes the search with either the specified position for the normal
+// search or the position one ply trimmed for the ponder search.
+void Engine::InitializeSearchPosition(bool for_ponder) {
+  assert(last_position_);
+  if (!for_ponder) {
+    search_->SetPosition(*last_position_);
+    return;
+  }
+  if (last_position_->moves.empty()) {
+    throw Exception("Ponder search requires at least one move.");
+  }
+  GameState position = *last_position_;
+  position.moves.pop_back();
+  search_->SetPosition(position);
+  return;
+}
+
 void Engine::SetPosition(const std::string& fen,
                          const std::vector<std::string>& moves) {
-  UpdateBackendConfig();
   EnsureSearchStopped();
+  ponder_enabled_ = options_.Get<bool>(kPonderId);
+  strict_uci_timing_ = options_.Get<bool>(kStrictUciTiming);
+  if (!strict_uci_timing_) search_->StartClock();
+  UpdateBackendConfig();
   EnsureSyzygyTablebasesLoaded();
-  search_->SetPosition(MakeGameState(fen, moves));
-  search_initialized_ = true;
+  last_position_ = MakeGameState(fen, moves);
+  if (!ponder_enabled_) InitializeSearchPosition(/*for_ponder=*/false);
 }
 
 void Engine::NewGame() {
@@ -134,18 +224,36 @@ void Engine::NewGame() {
 }
 
 void Engine::Go(const GoParams& params) {
-  if (!search_initialized_) NewGame();
+  if (!ponder_enabled_ && params.ponder) {
+    throw Exception(
+        "Ponder is not enabled, but the ponder search is requested.");
+  }
+  if (strict_uci_timing_) search_->StartClock();
+  if (!last_position_) NewGame();
+  if (ponder_enabled_) InitializeSearchPosition(params.ponder);
+  last_go_params_ = params;
   search_->StartSearch(params);
 }
 
 void Engine::Stop() { search_->StopSearch(); }
 
+void Engine::PonderHit() {
+  if (!last_go_params_ || !last_go_params_->ponder) {
+    throw Exception("ponderhit while not pondering");
+  }
+  EnsureSearchStopped();
+  search_->StartClock();
+  last_go_params_->ponder = false;
+  InitializeSearchPosition(/*ponder=*/false);
+  search_->StartSearch(*last_go_params_);
+}
+
 void Engine::RegisterUciResponder(UciResponder* responder) {
-  uci_forwarder_.Register(responder);
+  uci_forwarder_->Register(responder);
 }
 
 void Engine::UnregisterUciResponder(UciResponder* responder) {
-  uci_forwarder_.Unregister(responder);
+  uci_forwarder_->Unregister(responder);
 }
 
 }  // namespace lczero
