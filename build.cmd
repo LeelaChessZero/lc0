bb901577384b8cd288ad00689979e2ba0503ee19
Jamie Huta <118079848+xfearofdarkness@users.noreply.github.com>
2025-07-03 13:18:29 +0200

    Improve windows build script (#2196)
    * feat: Improve build.cmd for automatic CUDA detection

--- build.cmd
+++ build.cmd
@@ -12,14 +12,22 @@ set OPENBLAS=false
 set EIGEN=false
 set TEST=false
 
+if "%CUDA%"=="true" (
+  if not defined CUDA_PATH (
+    echo WARNING: CUDA_PATH environment variable not found. Using default value.
+  ) else (
+    echo CUDA_PATH found in system environment: "%CUDA_PATH%"
+  )
+)
+
 rem 2. Edit the paths for the build dependencies.
-set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0
+if not defined CUDA_PATH set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9
 set CUDNN_PATH=%CUDA_PATH%
+set OPENCL_LIB_PATH=%CUDA_PATH%\lib\x64
+set OPENCL_INCLUDE_PATH=%CUDA_PATH%\include
 set OPENBLAS_PATH=C:\OpenBLAS
 set MKL_PATH=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl
 set DNNL_PATH=C:\dnnl_win_1.1.1_cpu_vcomp
-set OPENCL_LIB_PATH=%CUDA_PATH%\lib\x64
-set OPENCL_INCLUDE_PATH=%CUDA_PATH%\include
 
 rem 3. In most cases you won't need to change anything further down.
 echo Deleting build directory:
