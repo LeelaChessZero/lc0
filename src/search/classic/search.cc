2ddf1a8cdb1f17ce44f0e03ecf155cee44e23fdb
Menkib <menkib64@proton.me>
2025-08-29 02:36:36 +0300

    Silence thread sanitizer warnings in search (#2270)
    libstdc++ include bound checking code when using vector::operator[].
    This generates thread sanitizer warnings about reading then the end
    pointer of vector.

--- src/search/classic/search.cc
+++ src/search/classic/search.cc
@@ -1105,7 +1105,7 @@ void SearchWorker::RunTasks(int tid) {
             // We got the spin lock, double check we're still in the clear.
             if (nta < tc) {
               id = tasks_taken_.fetch_add(1, std::memory_order_acq_rel);
-              task = &picking_tasks_[id];
+              task = picking_tasks_.data() + id;
               task_taking_started_.store(0, std::memory_order_release);
               break;
             }
@@ -1153,7 +1153,7 @@ void SearchWorker::RunTasks(int tid) {
           break;
         }
       }
-      picking_tasks_[id].complete = true;
+      picking_tasks_.data()[id].complete = true;
       completed_tasks_.fetch_add(1, std::memory_order_acq_rel);
     }
   }
