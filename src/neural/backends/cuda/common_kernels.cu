b01d550bd83fd09de63d44ebc42c4be5e10b018f
borg323 <borg323@users.noreply.github.com>
2025-08-30 15:39:38 +0300

    guard against inf fp16 input in softmax kernel

--- src/neural/backends/cuda/common_kernels.cu
+++ src/neural/backends/cuda/common_kernels.cu
@@ -808,6 +808,12 @@ void OutputInputTransform(int N, int C, int se_K, T* output, const T* input,
   ReportCUDAErrors(cudaGetLastError());
 }
 
+__device__ __forceinline__ float clamp(float val, float low, float high) {
+  if (val > high) return high;
+  if (val < low) return low;
+  return val;
+}
+
 // softmax along C dimension which is assumed to be 64
 // each thread processes two elements. Each warp computes a sum (over 64
 // elements)
@@ -842,6 +848,11 @@ __global__ void softmax_opt_64_kernel(T* output, const T* input,
   if (input2 != nullptr) {
     x[0] += x[2];
     x[1] += x[3];
+  }
+  if (fp16) {
+    // Guard against Inf: 1e6 is outside the fp16 range.
+    x[0] = clamp(x[0], -1e6, 1e6);
+    x[1] = clamp(x[1], -1e6, 1e6);
   }
   float threadMax = max(x[0], x[1]);
   float maxval = warpMax(threadMax);
@@ -884,6 +895,10 @@ __global__ void softmax_kernel(T* output, const T* input, const T* input2) {
 
   float x = (float)input[index];
   if (input2 != nullptr) x += (float)input2[index];
+  if (std::is_same<half, T>::value) {
+    // Guard against Inf: 1e6 is outside the fp16 range.
+    x = clamp(x, -1e6, 1e6);
+  }
 
   __shared__ float sum, maxval;
   if (c == 0) {
