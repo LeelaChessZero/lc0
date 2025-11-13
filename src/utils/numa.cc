321568ba15f2632827addaca0fdb7c7474ef31de
Menkib <menkib64@proton.me>
2025-11-13 17:19:23 +0200

    Try to go back to has_function check

--- src/utils/numa.cc
+++ src/utils/numa.cc
@@ -25,9 +25,9 @@
   Program grant you additional permission to convey the resulting work.
 */
 
-#define _GNU_SOURCE
-#include <pthread.h>
 #include "utils/numa.h"
+
+#include <pthread.h>
 
 #include <bitset>
 
