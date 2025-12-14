dfd82d33eedd7d6367219f9030fa4358fc1a2757
borg323 <borg323@users.noreply.github.com>
2025-12-14 02:05:47 +0200

    fixes

--- scripts/appveyor_win_build.cmd
+++ scripts/appveyor_win_build.cmd
@@ -22,6 +22,7 @@ IF %PGO%==true msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimiz
 IF %NAME%==onnx (
   ren build\lc0.exe lc0-trt.exe
   meson configure build -Ddefault_backend= -Dcudnn_libdirs= -Dgtest=%GTEST%
+  msbuild "C:\projects\lc0\build\lc0.sln" /target:REGEN
   IF %PGO%==true msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=PGOptimize /p:DebugInformationFormat=ProgramDatabase /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   IF %PGO%==false msbuild "C:\projects\lc0\build\lc0.sln" /m /p:WholeProgramOptimization=true /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   ren build\lc0.exe lc0-dml.exe
