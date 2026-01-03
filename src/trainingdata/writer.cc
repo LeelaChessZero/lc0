79c849ad381073b09e9d0160e471f163f7824d90
Viet-Anh Tran <vietanh1423@gmail.com>
2026-01-03 07:27:31 +0000

    Allow to pass a flag to specify the output path for TrainingDataWriter

--- src/trainingdata/writer.cc
+++ src/trainingdata/writer.cc
@@ -45,9 +45,13 @@ std::string GetLc0CacheDirectory() {
 
 }  // namespace
 
-TrainingDataWriter::TrainingDataWriter(int game_id) {
-  static std::string directory =
+TrainingDataWriter::TrainingDataWriter(int game_id, std:string folder) {
+  static std::string sDirectory =
       GetLc0CacheDirectory() + "data-" + Random::Get().GetString(12);
+
+  std::string directory = folder.empty()
+                              ? sDirectory
+                              : CommandLine::BinaryDirectory() + "/" + folder;
   // It's fine if it already exists.
   CreateDirectory(directory.c_str());
 
