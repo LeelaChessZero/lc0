e074660692b0ab97690eb226aee593c4b96bda1b
Menkib <menkib64@proton.me>
2026-03-04 17:09:22 +0200

    Fix policy decay visit lower limit

--- src/search/classic/params.cc
+++ src/search/classic/params.cc
@@ -571,7 +571,7 @@ void BaseSearchParams::Populate(OptionsParser* options) {
   options->Add<FloatOption>(kCpuctFactorAtRootId, 0.0f, 1000.0f) = 3.894f;
   options->Add<BoolOption>(kRootHasOwnCpuctParamsId) = false;
   options->Add<FloatOption>(kPolicyValueTemperatureId, 1e-15f, 10.0f) = 0.0148f;
-  options->Add<IntOption>(kPolicyDecayVisitsId, 0, 100000000) = 3500;
+  options->Add<IntOption>(kPolicyDecayVisitsId, 1, 100000000) = 3500;
   options->Add<FloatOption>(kPolicyDecayValueShareId, 0.0f, 100.0f) = 100.0f;
   options->Add<BoolOption>(kTwoFoldDrawsId) = true;
   options->Add<FloatOption>(kTemperatureId, 0.0f, 100.0f) = 0.0f;
